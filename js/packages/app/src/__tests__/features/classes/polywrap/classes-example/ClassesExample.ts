0815f611fad817198bf7054c109bad39447f05cc
Jure Bogunovic <bogunovij@gmail.com>
2022-11-04 00:06:50 +0100

    refactored manual app codegen for classes

--- js/packages/app/src/__tests__/features/classes/polywrap/classes-example/ClassesExample.ts
+++ js/packages/app/src/__tests__/features/classes/polywrap/classes-example/ClassesExample.ts
@@ -1,39 +1,10 @@
 import { IWrapper } from "@polywrap/reim-wrap";
-import { TestInstanceMethodArgs } from "./TestInstanceMethodArgs";
+import { create as createTestClass } from "./internal-types/TestClass";
 
 export class ClassesExample {
   static from(wrapper: IWrapper) {
     return {
-      TestClass: {
-        async create(arg: string): Promise<TestClass> {
-          const objectReferencePtr = await wrapper.invokeClassMethod<TestInstanceMethodArgs, number>("TestClass", "create", { arg });
-
-          console.log("create resultaaa: ", objectReferencePtr);
-          return new TestClass(wrapper, objectReferencePtr);
-        },
-        testStaticMethod(arg: string): Promise<string> {
-          return wrapper.invokeClassMethod<TestInstanceMethodArgs, string>("TestClass", "testStaticMethod", { arg });
-        }
-      }
+      TestClass: createTestClass(wrapper),
     };
-  }
-}
-
-export class TestClass {
-  constructor(private readonly __wrapper: IWrapper, private readonly __objectReferencePtr: number) {
-  }
-
-  testInstanceMethod(arg: string): Promise<string> {
-    return this.__wrapper.invokeClassMethod<{
-      objectReferencePtr: number;
-      args: TestInstanceMethodArgs;
-    }, string>(
-      "TestClass", 
-      "testInstanceMethod", 
-      { 
-        objectReferencePtr: this.__objectReferencePtr, 
-        args: { arg } 
-      }
-    );
   }
 }
