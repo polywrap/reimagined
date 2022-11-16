bc8d5149cd10e9a6ce0bdc3526529bd5ac12ad47
Jure Bogunovic <bogunovij@gmail.com>
2022-11-16 18:59:57 +0100

    added rust support to reim cli and fixed issues with reim_wrap

--- rust/reim_wrap/src/wrapper_module.rs
+++ rust/reim_wrap/src/wrapper_module.rs
@@ -25,12 +25,14 @@ impl WrapperModule {
 
 struct ReceiverWithModule {
     internal_module: Arc<Mutex<dyn InternalModule>>,
+    external_module: Arc<dyn ExternalModule>,
 }
 
 impl ReceiverWithModule {
-    pub fn new(internal_module: Arc<Mutex<dyn InternalModule>>) -> Self {
+    pub fn new(internal_module: Arc<Mutex<dyn InternalModule>>, external_module: Arc<dyn ExternalModule>) -> Self {
         Self {
             internal_module,
+            external_module,
         }
     }
 }
@@ -43,21 +45,23 @@ impl Receiver for ReceiverWithModule {
         let resource = u32::from_be_bytes(buffer.try_into().expect("Resource must be 4 bytes"));
         let data_buffer = &buffer[4..];
 
-        internal_module.invoke_resource(resource, data_buffer).await
+        internal_module.invoke_resource(resource, data_buffer, Arc::clone(&self.external_module)).await
     }
 }
 
 #[async_trait]
 impl ExternalModule for WrapperModule {
-    async fn invoke_resource(&mut self, resource: u32, buffer: &[u8]) -> Vec<u8> {
+    async fn invoke_resource(self: Arc<Self>, resource: u32, buffer: &[u8]) -> Vec<u8> {
         let x = self.internal_module.clone();
         let mut dt_instance = self.dt_instance.lock().await;
         
+        let y = Arc::clone(&self);
         dt_instance.send(
             &[&resource.to_be_bytes(), buffer].concat(),
             Arc::new(
                 ReceiverWithModule::new(
-                    x
+                    x,
+                    y
                 )
             )
         )
